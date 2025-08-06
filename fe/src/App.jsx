3a78d1715596d806c250dae2bf11b5bd2d5033d0
rizkyzul <kyzk23@gmail.com>
2025-08-07 01:32:20 +0700

    add animate from aos

--- fe/src/App.jsx
+++ fe/src/App.jsx
@@ -16,9 +16,21 @@ import Footer from './components/Footer';
 import StrukturOrganisasiPage from './pages/StrukturOrganisasiPage'; // <-- Pastikan ini diimpor
 import ScrollToHashElement from './utils/ScrollToHashElement'; // ← import dulu
 import ScrollToTopButton from './components/ScrollToTopButton';
+// import AOS from 'aos';
+// import 'aos/dist/aos.css';
+// import { useEffect } from 'react';
+
 
 
 function App() {
+//   useEffect(() => {
+//   AOS.init({
+//     duration: 800,
+//     once: false,
+//     offset:100,
+//   });
+// }, []);
+
   return (
     <BrowserRouter>
       <ScrollToHashElement />
