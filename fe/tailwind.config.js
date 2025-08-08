3f9e1cdfb3e3ae142fe9776e5238f4eabac45881
rizkyzul <kyzk23@gmail.com>
2025-08-09 01:31:24 +0700

    editie

--- fe/tailwind.config.js
+++ fe/tailwind.config.js
@@ -3,23 +3,5 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
-  theme: {
-    extend: {
-      keyframes: {
-        moveLeft: {
-          '0%': { transform: 'translateX(100%)' },
-          '100%': { transform: 'translateX(-100%)' },
-        },
-        moveRight: {
-          '0%': { transform: 'translateX(-100%)' },
-          '100%': { transform: 'translateX(100%)' },
-        },
-      },
-      animation: {
-        'move-left': 'moveLeft 20s linear infinite',
-        'move-right': 'moveRight 20s linear infinite',
-      },
-    },
-  },
   plugins: [],
 }
